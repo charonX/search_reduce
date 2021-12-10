e13ed67e958bd140f465b349c628974becd21bde
I2O <charon.z@qq.com>
2021-12-10 18:09:31 +0800

    随机背景功能添加

--- engine/bg.js
+++ engine/bg.js
@@ -7,7 +7,21 @@ export default class Background{
         this.dom = document.getElementById('bg')
         this.btnRefresh = document.getElementById('refreshBg')
         this.config = {}
+        this.option = {
+            type:'bing'
+        }
+        this.Loading = false
         this.__init()
+        this.__addEvent()
+    }
+
+    __addEvent(){
+        
+        this.btnRefresh.addEventListener('click',()=>{
+            if(this.Loading) return
+            
+            this.__checkBackground()
+        })
     }
     async __init(){
         let storage = await Get(this.name)
@@ -25,6 +39,7 @@ export default class Background{
     }
 
     __setDom(imgData){
+        this.Loading = false
         this.dom.className = 'animate__animated animate__fadeIn'
         this.dom.style.backgroundImage = `url(${imgData})`
     }
@@ -42,22 +57,63 @@ export default class Background{
         return `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
     }
 
-    __checkBackground(){
-        // 未来判断背景图类型设置用
-        // 背景图网站
-        // https://wallhaven.cc/latest?page=2
-        this.__getBackground()
+    async __checkBackground(){
+        this.Loading = true
+        let url = ''
+        switch(this.option.type){
+            case 'bing':
+                url =await this.__getBingBG()
+                break
+            case 'random':
+                url = await this.__getRandomBG()
+                break
+        }
+
+        this.__getBackground(url)
+       
     }
 
-    async __getBackground(){
+    async __getBackground(url){
         try{
-            let url = await this.__getBingBG()
             let imgData = await getImgDataURI(url)
             this.__setDom(imgData)
             this.__setStorage(imgData)
         }catch(e){
-            console.error(e)
+            
         }
+    }
+    __getRandomBG(){
+        let url = 'https://wallhaven.cc/search?categories=110&purity=100&ratios=16x9%2C21x9%2C16x10&sorting=random&order=desc'
+        return new Promise((resolve, reject) => {
+            chrome.runtime.sendMessage(
+                {
+                    messageType: 'fetch',
+                    url,
+                },
+                (res) => {
+                    if(res.success){
+                        let parser = new DOMParser();
+                        let doc_dom = parser.parseFromString(res.data, "text/html");
+                        let img_wrap = doc_dom.querySelector('.thumb-listing-page figure')
+                        let img_min_url = img_wrap.querySelector('img').getAttribute('data-src')
+                        img_min_url = img_min_url.replace('https://th.wallhaven.cc/small/', '')
+                        img_min_url = img_min_url.replace('.jpg', '')
+                        const [space,name] = img_min_url.split('/')
+                        let isPng = img_wrap.querySelector('.png')
+
+                        let imgUrl =  `https://w.wallhaven.cc/full/${space}/wallhaven-${name}${!!isPng ? '.png' : '.jpg'}`
+                        if(imgUrl){
+                            resolve(imgUrl)
+                        }else{
+                            reject('dom未找到')
+                        }
+                        
+                    }else{
+                        reject(res.err)
+                    }
+                }
+            );
+        })
     }
     __getBingBG(){
         let url = 'https://www.bing.com'
