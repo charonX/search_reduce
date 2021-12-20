31d78835f8a9946ab9e71543d59111029fe8a4e5
charonX <charon.z@qq.com>
2021-12-20 23:13:56 +0800

    init web compontnet modal

---
+++ components/modal.js
@@ -0,0 +1,28 @@
+class Modal extends HTMLElement {
+    constructor() {
+        super();
+        let templete = `<template id="m-modal">
+            <style></style>
+            <div class="modal active" id="options" data-index="">
+                <a class="modal-overlay" href="#modals-sizes" aria-label="Close"></a>
+                <div class="modal-container" role="document">
+                <div class="modal-header"><a class="btn btn-clear float-right" href="#modals-sizes" aria-label="Close"></a>
+                    <div class="modal-title h5">标题</div>
+                </div>
+                <div class="modal-body">
+                    <slot name="modal-body"></slot>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" aria-label="Submit">提交</button><a class="btn btn-link" href="#modals-sizes" aria-label="Close">取消</a>
+                </div>
+                </div>
+            </div>
+        </template>`
+        let wrap = document.createElement('div');
+        wrap.innerHTML = templete
+        let shadow = this.attachShadow({mode: 'open'});
+        shadow.appendChild(wrap.firstChild.content);
+    }
+  }
+
+  customElements.define('m-modal', Modal);
